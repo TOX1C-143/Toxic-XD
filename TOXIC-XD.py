6fddc231685a3dfb817c5784ab0813186c4d3ca3
Ayan_ahammed <99079519+TOX1C-143@users.noreply.github.com>
2024-01-30 01:35:54 +0600

    Delete TOXIC-XD.py

--- TOXIC-XD.py
+++
@@ -1,9 +0,0 @@
-import platform
-import os
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("xxx")._login()
-elif 'aarch' in arc:
-	__import__("Toxic").asyncio.run(approval())
-else:
-	exit(f' Unknow device machine {arc}')
